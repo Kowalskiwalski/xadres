36a5354c25a32a1a406fe0947013d2c2b8d4f4a4
Lucas Ferreira <lucasfb159@gmail.com>
2017-11-29 09:55:46 -0200

    Projeto criado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+namespace Xadrez {
+    class Program {
+        static void Main(string[] args) {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+
+        }
+    }
+}
