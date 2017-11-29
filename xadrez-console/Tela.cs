2447714109daf38985dc77ef5390efdf2b741c5c
Lucas Ferreira <lucasfb159@gmail.com>
2017-11-29 14:50:28 -0200

    Melhorando a implementacao do metodo imprimirTabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -6,6 +6,7 @@ namespace xadrez_console {
         public static void imprimirTabuleiro(Tabuleiro tab) {
 
             for (int i=0; i<tab.linhas; i++) {
+                Console.Write(8 - i + " ");
                 for (int j=0; j<tab.colunas; j++) {
                     if (tab.peca(i, j) == null) {
                         Console.Write("- ");
@@ -16,6 +17,9 @@ namespace xadrez_console {
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
         }
+
+        public static void
     }
 }
